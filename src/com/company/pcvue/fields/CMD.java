64e02b91c3449a42949ed4475b300fc170d264cd
Stephen <cheunste@gmail.com>
2018-05-20 12:47:10 -0700

    Added new class CMD

---
+++ src/com/company/pcvue/fields/CMD.java
@@ -0,0 +1,58 @@
+package com.company.pcvue.fields;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Stephen on 5/20/2018.
+ */
+public class CMD extends VarexpVariable {
+    public ArrayList<List<String>> cmdList;
+
+    public CMD() {
+        this.cmdList = new ArrayList<>();
+        setTableName("cmd");
+    }
+
+    @Override
+    void VarexpVariable() {
+
+    }
+
+    @Override
+    String insertToDB() {
+        return null;
+    }
+
+    @Override
+    String empty() {
+        String emptyString = "";
+        for (int i = 40; i <= 42; i++) {
+            emptyString += ",";
+        }
+
+        emptyString += ",";
+        return emptyString;
+    }
+
+    @Override
+    String getJoinCmd() {
+        return "RIGHT JOIN cmd on common.variable_id = cmd.cmd_variable_id";
+    }
+
+    @Override
+    public void setArrayList(String varexpString, int dbIndex) {
+        setvarexpArrayList(varexpString);
+        List<String> cmdList = new ArrayList<>();
+        List<String> varexpArraySplit = this.getVarexpList();
+
+        cmdList.add("" + dbIndex);
+        for (int i = 40; i <= 43; i++) {
+            cmdList.add(varexpArraySplit.get(i).toString());
+        }
+
+        cmdList.add(varexpArraySplit.get(156).toString());
+
+        this.cmdList.add(cmdList);
+    }
+}
