ac2d6b493a31195de9e44e17fe058d6a00fee6c3
Stephen <cheunste@gmail.com>
2018-05-20 12:44:00 -0700

    Added new class CHR

---
+++ src/com/company/pcvue/fields/CHR.java
@@ -0,0 +1,65 @@
+package com.company.pcvue.fields;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Stephen on 5/20/2018.
+ */
+public class CHR extends VarexpVariable {
+
+    public ArrayList<List<String>> chrList;
+
+    public CHR() {
+        this.chrList = new ArrayList<>();
+        setTableName("chr");
+    }
+
+    @Override
+    void VarexpVariable() {
+
+    }
+
+    @Override
+    String insertToDB() {
+        return null;
+    }
+
+    @Override
+    String empty() {
+        String emptyString = "";
+        for (int i = 59; i <= 66; i++) {
+            emptyString += ",";
+        }
+        for (int i = 89; i <= 94; i++) {
+            emptyString += ",";
+        }
+        emptyString += ",";
+        emptyString += ",";
+        return emptyString;
+    }
+
+    @Override
+    String getJoinCmd() {
+        return null;
+    }
+
+    @Override
+    public void setArrayList(String varexpString, int dbIndex) {
+        setvarexpArrayList(varexpString);
+        List<String> chrList = new ArrayList<>();
+        List<String> varexpArraySplit = this.getVarexpList();
+
+        chrList.add("" + dbIndex);
+        for (int i = 59; i <= 66; i++) {
+            chrList.add(varexpArraySplit.get(i).toString());
+        }
+        for (int i = 89; i <= 94; i++) {
+            chrList.add(varexpArraySplit.get(i).toString());
+        }
+        chrList.add(varexpArraySplit.get(155).toString());
+        chrList.add(varexpArraySplit.get(156).toString());
+
+        this.chrList.add(chrList);
+    }
+}
