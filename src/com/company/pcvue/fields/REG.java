e653477dae817c8010eed7ec0d039b95488fbb52
Stephen <cheunste@gmail.com>
2018-05-20 13:10:34 -0700

    Added new class REG

---
+++ src/com/company/pcvue/fields/REG.java
@@ -0,0 +1,59 @@
+package com.company.pcvue.fields;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Stephen on 5/20/2018.
+ */
+public class REG extends VarexpVariable {
+    public ArrayList<List<String>> regList;
+
+    public REG() {
+        this.regList = new ArrayList<>();
+        setTableName("reg");
+    }
+
+    @Override
+    void VarexpVariable() {
+
+    }
+
+    @Override
+    String insertToDB() {
+        return null;
+    }
+
+    @Override
+    String empty() {
+        String emptyString = "";
+        for (int i = 59; i <= 66; i++) {
+            emptyString += ",";
+        }
+        emptyString += ",";
+        emptyString += ",";
+        return emptyString;
+    }
+
+    @Override
+    String getJoinCmd() {
+        return "RIGHT JOIN reg on common.variable_id = reg.reg_variable_id";
+    }
+
+    @Override
+    public void setArrayList(String varexpString, int dbIndex) {
+        setvarexpArrayList(varexpString);
+        List<String> acmList = new ArrayList<>();
+        List<String> varexpArraySplit = this.getVarexpList();
+
+        acmList.add("" + dbIndex);
+        for (int i = 59; i <= 66; i++) {
+            acmList.add(varexpArraySplit.get(i).toString());
+        }
+
+        acmList.add(varexpArraySplit.get(155).toString());
+        acmList.add(varexpArraySplit.get(156).toString());
+
+        this.regList.add(acmList);
+    }
+}
