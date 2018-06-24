dbea1eed2d0f9638ba26051af11a431e1c472a2e
Stephen <cheunste@gmail.com>
2018-06-24 15:12:32 -0700

    Created a factory class, VarexpFactory (I really need a better name) that instaniates a varexp variable. This way, other classes don't need to waste their time creating multiple instances of a class

---
+++ src/com/company/pcvue/fields/VarexpFactory.java
@@ -0,0 +1,102 @@
+package com.company.pcvue.fields;
+
+/**
+ * This is a class that is responsible for creating references to other varexp variables without the other
+ * actually interfacting with any of them
+ * <p>
+ * This is a factory class
+ */
+public class VarexpFactory {
+
+    public VarexpVariable declareNewClass(String newVarexpVariable) {
+        VarexpVariable varexpVariable = null;
+
+        switch (newVarexpVariable) {
+            case "ACM":
+                return new ACM();
+
+            case "ALA":
+                return new ALA();
+
+            case "ATS":
+                return new ATS();
+
+            case "BIT":
+                return new BIT();
+
+            case "CHR":
+                return new CHR();
+
+            case "CMD":
+                return new CMD();
+
+            case "CNT":
+                return new CNT();
+
+            case "CTV":
+                return new CTV();
+
+            case "CXT":
+                return new CXT();
+
+            case "REG":
+                return new REG();
+
+            case "TXT":
+                return new TXT();
+
+            case "TSH":
+                return new TSH();
+
+            //For equpiment
+            case "E":
+                return new Equipment();
+
+            //For Internal
+            case "I":
+                return new Internal();
+
+            //For external
+            case "X":
+                return new External();
+
+            //For DDE
+            case "D":
+                return new DDE();
+
+            //For OPC
+            case "O":
+                return new OPC();
+
+            //For Lonwork
+            case "L":
+                return new LonWork();
+
+            //For BACnet
+            case "B":
+                return new BACnet();
+
+            //For 60870-5-104
+            case "4":
+                return new IEC60870_Master();
+
+            //For 61850
+            case "8":
+                return new IEC61850_Master();
+
+            //For DNP3
+            case "3":
+                return new DNP3_Master();
+
+            //For SNMP Manager
+            case "S":
+                return new SNMP();
+
+            default:
+                //Handle other unexpect cases here
+                return null;
+
+        }
+    }
+
+}
