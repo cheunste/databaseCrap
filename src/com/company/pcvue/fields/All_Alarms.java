1623cbcac63fb2b38bd4aeddf6ae75f90daca915
Stephen <cheunste@gmail.com>
2018-06-24 08:23:32 -0700

    Refactored ALA, All_Alarms, ATS

--- src/com/company/pcvue/fields/All_Alarms.java
+++ src/com/company/pcvue/fields/All_Alarms.java
@@ -12,13 +12,19 @@ public class All_Alarms extends VarexpVariable {
     public All_Alarms() {
         this.allAlarmsList = new ArrayList<>();
         setTableName("all_alarms");
+        setPositionList();
 
     }
 
 
     @Override
     void setPositionList() {
-
+        for (int i = 158; i <= 160; i++) {
+            varexpPositionList.add(i);
+        }
+        for (int i = 175; i <= 177; i++) {
+            varexpPositionList.add(i);
+        }
     }
 
     @Override
@@ -29,10 +35,7 @@ public class All_Alarms extends VarexpVariable {
     @Override
     String empty() {
         String emptyString = "";
-        for (int i = 158; i <= 160; i++) {
-            emptyString += ",";
-        }
-        for (int i = 175; i <= 177; i++) {
+        for (int i : varexpPositionList) {
 
             emptyString += ",";
         }
@@ -55,13 +58,9 @@ public class All_Alarms extends VarexpVariable {
         List<String> varexpArraySplit = this.getVarexpList();
 
         all_alarmsList.add("" + dbIndex);
-        for (int i = 158; i <= 160; i++) {
-            all_alarmsList.add(varexpArraySplit.get(i).toString());
+        for (int i : varexpPositionList) {
+            all_alarmsList.add(varexpArraySplit.get(i));
         }
-        for (int i = 175; i <= 177; i++) {
-            all_alarmsList.add(varexpArraySplit.get(i).toString());
-        }
-
         this.allAlarmsList.add(all_alarmsList);
     }
 }
