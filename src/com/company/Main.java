d4b6421a81e9c81c710a70e17820dbb67a94c7dd
Stephen <cheunste@gmail.com>
2018-07-16 15:21:06 -0700

    Updated the Main class so it handles flags and parses. Also performed a cleanup and parameter changes.
    Also updated MainTest.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,33 +1,27 @@
 package com.company;
 
 import com.company.Database.*;
-import com.company.pcvue.fields.VarexpFactory;
-import com.company.pcvue.fields.VarexpVariable;
-import com.sun.org.apache.bcel.internal.generic.ARRAYLENGTH;
 
 import java.io.*;
 import java.sql.*;
-import java.util.ArrayList;
+import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Main {
 
     public static void importFile(String fileLocation, String databaseName) throws IOException, SQLException {
-            deleteDB(databaseName);
-            createDB(databaseName);
 
+        Buffer buffer = new Buffer();
+        ExecutorService executor = Executors.newFixedThreadPool(2);
 
-            Buffer buffer = new Buffer();
-            ExecutorService executor = Executors.newFixedThreadPool(2);
-
-            //This is the consumer. It consumes data in the queue
-            executor.execute(new ImportHandler(buffer, databaseName));
-            //This will be the producer (see producer-consumer problem if you're not familiar with hte term)
+        //This is the consumer. It consumes data in the queue
+        executor.execute(new ImportHandler(buffer, databaseName));
+        //This will be the producer (see producer-consumer problem if you're not familiar with hte term)
         executor.execute(new Import(fileLocation, databaseName, buffer));
 
-            //Shtudown
-            executor.shutdown();
+        //Shtudown
+        executor.shutdown();
     }
 
     public static void deleteDB(String databaseName) {
@@ -44,22 +38,78 @@ public class Main {
 
     public static void main(String[] args) throws IOException, ArrayIndexOutOfBoundsException, SQLException {
 
-        String filePath = args[0];
-        String dbName = args[1];
+        //String command = args[0];
+        //String filePath = args[1];
+        //String dbName = args[2];
 
         //importFile(filePath,dbName);
-        Buffer buffer = new Buffer();
-        Export exp = new Export(dbName, buffer);
-        exp.exportDB();
+
+        final Map<String, List<String>> params = new HashMap<>();
+
+        List<String> options = null;
+        ;
+        for (int i = 0; i < args.length; i++) {
+            final String a = args[i];
+
+            if (a.charAt(0) == '-') {
+                if (a.length() < 2) {
+                    System.err.println("Error at argument " + a);
+                    return;
+                }
+
+                options = new ArrayList<>();
+                params.put(a.substring(1), options);
+            } else if (options != null) {
+                options.add(a);
+            } else {
+                System.err.println("Illegal parameter usage");
+                System.out.println("Usage: [Command] [path to file] [databaseName]");
+            }
+        }
+
+        for (Map.Entry<String, List<String>> entry : params.entrySet()) {
+            String key = entry.getKey();
+            List<String> value = entry.getValue();
+
+            System.out.println(key);
+            System.out.println(value);
+            switch (key) {
+
+                //Import
+                case "I":
+                    if (value.size() != 2) {
+                        System.err.println("Error: Import Command only accepts two arguments. Please try again");
+                        System.out.println("Usage: -I [file path] [database Name]");
+
+                    } else {
+                        String databaseName = value.get(1);
+                        String filePath = value.get(0);
+                        deleteDB(databaseName);
+                        createDB(databaseName);
+                        importFile(filePath, databaseName);
+                    }
+
+                    break;
+                case "E":
+                    if (value.size() != 2) {
+                        System.err.println("Error: Export Command only accepts two arguments. Please try again");
+                        System.out.println("Usage: -E [Output File Path ] [Export DB Name]");
+                    } else {
+                        String databaseName = value.get(1);
+                        String filePath = value.get(0);
+                        exportFile(databaseName, filePath);
+                    }
+                    break;
+                default:
+                    System.err.println("Unknown Error");
+                    break;
+            }
+        }
 
     }
 
-    //Removes '[', ']' and removes all empty spaces after a single comma
-    private static String removeUnwantedCharacters(String varexpVariable) {
-        return varexpVariable.replace("[", "").replace("]", "").replaceAll(",\\ *", ",");
-    }
 
-    private void exportFile(String databaseName) {
+    public static void exportFile(String databaseName, String outputFilePath) {
 
         //Buffer buffer = new Buffer();
         //ExecutorService executor = Executors.newFixedThreadPool(2);
@@ -70,6 +120,9 @@ public class Main {
         //executor.execute(new Import(args, buffer));
 
         //Shtudown
+        Buffer buffer = new Buffer();
+        Export exp = new Export(databaseName, buffer, outputFilePath);
+        exp.exportDB();
     }
 
 }
--- src/com/company/test/MainTest.java
+++ src/com/company/test/MainTest.java
@@ -1,5 +1,15 @@
 package com.company.test;
 
+import com.company.Database.ImportHandler;
+import com.company.Main;
+import com.company.fileHandler;
+import com.mysql.cj.util.StringUtils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -7,7 +17,56 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class MainTest {
     @org.junit.jupiter.api.Test
-    void importFile() {
+    void compareTest() throws IOException, ArrayIndexOutOfBoundsException, SQLException {
+        Main.exportFile("twin_buttes_2", "C:\\Users\\Stephen\\Desktop\\varexp tool project");
+        String originalLine;
+        String exportLine;
+        String originalVarexpPath = "C:\\Users\\Stephen\\Desktop\\varexp tool project\\TWIN2_Varexp.txt";
+        String exportVarexpPath = "C:\\Users\\Stephen\\Desktop\\varexp tool project\\Test-Varexp.csv";
+        fileHandler fh = new fileHandler();
+        //open file
+        BufferedReader originalFileBR = fh.readInput(originalVarexpPath);
+        BufferedReader exportFileBR = fh.readInput(exportVarexpPath);
+        int temp = 0;
+        PrintWriter outputFile;
+        outputFile = new PrintWriter("C:\\Users\\Stephen\\Desktop\\varexp tool project\\diff.csv", "UTF-8");
+        String headerRow = "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126,127,128,129,130,131,132,133,134,135,136,137,138,139,140,141,142,143,144,145,146,147,148,149,150,151,152,153,154,155,156,157,158,159,160,161,162,163,164,165,166,167,168,169,170,171,172,173,174,175,176,177,178,179,180,181,182,183,184,185,186,187,188,189,190,191,192,193,194,195,196,197,198,199,200,201,202,203,204,205,206,207,208,209,210,211,212,213,214,215,216,217,218,219,220,221,222,223,224,225,226,227,228,229,230,231,232,233,234,235,236,237,238,239,240,241,242,243,244,245,246,247,248,249,250,251,252,253,254,255,256,257,258,259,260,261,262,263,264,265,266,267,268,269,270,271,272,273,274,275,276,277,278,279,280";
+        outputFile.println(headerRow);
+        outputFile.println("Original is first");
+        outputFile.println("Export is on second");
+        while ((originalLine = originalFileBR.readLine()) != null) {
+            exportLine = exportFileBR.readLine();
+
+            if (originalLine.equals(exportLine)) {
+
+            } else {
+
+                String difference = org.apache.commons.lang3.StringUtils.difference(exportLine, originalLine);
+
+                if (difference.length() > 2) {
+                    //outputFile.println("original:");
+                    outputFile.println(originalLine);
+
+                    //outputFile.println("Export:");
+                    outputFile.println(exportLine);
+
+                    //outputFile.println("difference between original:");
+                    //outputFile.println(difference);
+                }
+                /*
+                outputFile.println("Original:");
+                outputFile.println(originalLine);
+                outputFile.println("Export:");
+                outputFile.println(exportLine);
+                */
+            }
+        }
+        //close file
+        fh.closeFile(originalFileBR);
+        fh.closeFile(exportFileBR);
+
+        outputFile.close();
+
     }
 
 }
