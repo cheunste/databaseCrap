31233ca173450a01861d849d8e92b443cae58b51
Stephen <cheunste@gmail.com>
2018-07-18 10:43:57 -0700

    Added logging in main. Also wrapped the importFile function with a check and verify DB overwrite.
    Also, I added in an option to drop a DB...simply because I do not give a shit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,30 +7,56 @@ import java.sql.*;
 import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.logging.*;
 
 public class Main {
 
-    public static void importFile(String fileLocation, String databaseName) throws IOException, SQLException {
+    //Function to import the varexp file. Ask a user if they want to overwrite a DB first and then actually import it
+    private static void importFile(String fileLocation, String databaseName) throws IOException, SQLException {
+
+        dbConnector dbc = new dbConnector();
+
+        boolean dbExists = dbc.verifyDBExists(databaseName);
+
+        if (dbExists) {
+
+            System.out.println("Database already exists. Overwrite? (Y/N)");
+            Scanner sc = new Scanner(System.in);
+            String choice = sc.next().toString().toLowerCase();
+            if (choice.equals("y")) {
+                deleteDB(databaseName);
+                createDB(databaseName);
+                importHelper(fileLocation, databaseName);
+            } else {
+                System.out.println("No change will be made. Exiting tool");
+            }
+        } else {
+            importHelper(fileLocation, databaseName);
+        }
+
+
+    }
+
+    //An assistant method to ImportHandler. This Does the actual work of importing the DB Really needs a better name
+    private static void importHelper(String fileLocation, String databaseName) throws IOException, SQLException {
 
         Buffer buffer = new Buffer();
         ExecutorService executor = Executors.newFixedThreadPool(2);
-
         //This is the consumer. It consumes data in the queue
         executor.execute(new ImportHandler(buffer, databaseName));
         //This will be the producer (see producer-consumer problem if you're not familiar with hte term)
         executor.execute(new Import(fileLocation, databaseName, buffer));
-
         //Shtudown
         executor.shutdown();
     }
 
-    public static void deleteDB(String databaseName) {
+    private static void deleteDB(String databaseName) {
         dbConnector db = new dbConnector();
         db.deleteDB(databaseName);
         db.close();
     }
 
-    public static void createDB(String databaseName) {
+    private static void createDB(String databaseName) {
         dbConnector db = new dbConnector();
         db.createDB(databaseName);
         db.close();
@@ -38,13 +64,12 @@ public class Main {
 
     public static void main(String[] args) throws IOException, ArrayIndexOutOfBoundsException, SQLException {
 
-        //String command = args[0];
-        //String filePath = args[1];
-        //String dbName = args[2];
-
-        //importFile(filePath,dbName);
-
+        //A map is used to store params
         final Map<String, List<String>> params = new HashMap<>();
+
+        //For logging
+        Logger logger = Logger.getLogger(Main.class.getName());
+
 
         List<String> options = null;
         ;
@@ -73,32 +98,46 @@ public class Main {
 
             System.out.println(key);
             System.out.println(value);
-            switch (key) {
+
+            String databaseName = value.get(1);
+            String filePath = value.get(0);
+            switch (key.toLowerCase()) {
 
                 //Import
-                case "I":
+                case "i":
                     if (value.size() != 2) {
+                        logger.severe("Erro: Not enough arugments in Import");
+                        logger.severe("Params given: " + value);
                         System.err.println("Error: Import Command only accepts two arguments. Please try again");
                         System.out.println("Usage: -I [file path] [database Name]");
 
                     } else {
-                        String databaseName = value.get(1);
-                        String filePath = value.get(0);
                         deleteDB(databaseName);
                         createDB(databaseName);
                         importFile(filePath, databaseName);
                     }
-
                     break;
-                case "E":
+                case "e":
                     if (value.size() != 2) {
+                        logger.severe("Erro: Not enough arugments in Export");
+                        logger.severe("Params given: " + value);
                         System.err.println("Error: Export Command only accepts two arguments. Please try again");
                         System.out.println("Usage: -E [Output File Path ] [Export DB Name]");
                     } else {
-                        String databaseName = value.get(1);
-                        String filePath = value.get(0);
+
                         exportFile(databaseName, filePath);
                     }
+                    break;
+                case "h":
+                    System.out.println("Usage: ");
+                    System.out.println("Import: -I [Input File path] [database Name]");
+                    System.out.println("Export: -E [Output File Path ] [Export DB Name]");
+                    System.out.println("Delete: -D [database Name]");
+                    break;
+
+                case "d":
+                    System.out.println("Deleteing DB");
+                    deleteDB(databaseName);
                     break;
                 default:
                     System.err.println("Unknown Error");
@@ -111,13 +150,6 @@ public class Main {
 
     public static void exportFile(String databaseName, String outputFilePath) {
 
-        //Buffer buffer = new Buffer();
-        //ExecutorService executor = Executors.newFixedThreadPool(2);
-
-        //This is the consumer. It consumes data in the queue
-        //executor.execute(new ImportHandler(buffer, databaseName));
-        //This will be the producer (see producer-consumer problem if you're not familiar with hte term)
-        //executor.execute(new Import(args, buffer));
 
         //Shtudown
         Buffer buffer = new Buffer();
