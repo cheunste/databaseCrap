46585a2a0b6210ca991e8cf037bd448d71461ab1
Stephen <cheunste@gmail.com>
2018-07-30 15:10:07 -0700

    Added Export function to the GUI

---
+++ src/com/company/controller/ImportController.java
@@ -0,0 +1,19 @@
+package com.company.controller;
+
+import javafx.fxml.Initializable;
+import javafx.stage.FileChooser;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by Stephen on 7/30/2018.
+ */
+public class ImportController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        //Create a file dialog
+        FileChooser fileDialog = new FileChooser();
+    }
+}
