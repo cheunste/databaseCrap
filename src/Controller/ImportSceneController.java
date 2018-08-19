ffc7f7ea37c5580952465f0c326b25c87cfe509b
Stephen <cheunste@gmail.com>
2018-08-19 10:35:50 -0700

    Modified file so that it imports a file.
    However, it doesn't do DB checking yet, so I have no idea how that'll go.

--- src/Controller/ImportSceneController.java
+++ src/Controller/ImportSceneController.java
@@ -1,5 +1,9 @@
 package Controller;
 
+import com.company.Database.Buffer;
+import com.company.Database.Import;
+import com.company.Database.ImportHandler;
+import com.company.Database.dbConnector;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
 import javafx.beans.binding.Bindings;
@@ -13,7 +17,10 @@ import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.File;
-
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 /**
  * Created by Stephen on 8/13/2018.
  */
@@ -40,6 +47,31 @@ public class ImportSceneController {
     @FXML
     private Label outputLabel;
 
+    //Function to import the varexp file. Ask a user if they want to overwrite a DB first and then actually import it
+    private static void importFile(String fileLocation, String databaseName) throws IOException, SQLException {
+
+        dbConnector dbc = new dbConnector();
+
+        dbc.createDB(databaseName);
+        //create a database
+        //TODO: Consider inserting another alert here
+        importHelper(fileLocation, databaseName);
+
+    }
+
+    //An assistant method to ImportHandler. This Does the actual work of importing the DB Really needs a better name
+    private static void importHelper(String fileLocation, String databaseName) throws IOException, SQLException {
+
+        Buffer buffer = new Buffer();
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        //This is the consumer. It consumes data in the queue
+        executor.execute(new ImportHandler(buffer, databaseName));
+        //This will be the producer (see producer-consumer problem if you're not familiar with hte term)
+        executor.execute(new Import(fileLocation, databaseName, buffer));
+        //Shtudown
+        executor.shutdown();
+    }
+
     //The following initializes all the buttons to their proper actions or functions
     @FXML
     private void initialize() {
@@ -58,7 +90,13 @@ public class ImportSceneController {
                 Bindings.or(importDatabaseName.textProperty().isEmpty(), outputLabel.textProperty().isEmpty())
         );
         importButton.setOnAction(e -> {
-
+            try {
+                importFile(outputLabel.getText(), importDatabaseName.getText());
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            } catch (SQLException e1) {
+                e1.printStackTrace();
+            }
         });
     }
 
